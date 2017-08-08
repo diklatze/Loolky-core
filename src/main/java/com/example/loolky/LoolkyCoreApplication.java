91e55995fe6953d6e2502cd970a0ca40e4ada236
dikla.weyl <dikla.weyl@diklaw-hrdt.fundtech.isr>
2017-08-08 09:51:43 +0300

    the beginning- Dikla

---
+++ src/main/java/com/example/loolky/LoolkyCoreApplication.java
@@ -0,0 +1,12 @@
+package com.example.loolky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoolkyCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoolkyCoreApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/loolky/user.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.example.loolky;
+
+/**
+ * @author dikla.weyl
+ *
+ */
+public class user {
+
+}
